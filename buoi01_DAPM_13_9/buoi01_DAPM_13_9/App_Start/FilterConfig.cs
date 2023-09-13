5a51f94847edb1e2ac5a7837d73982ea6d47f80c
Pontax <toan30112003@gmail.com>
2023-09-13 13:40:54 +0700

    project

---
+++ buoi01_DAPM_13_9/buoi01_DAPM_13_9/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace buoi01_DAPM_13_9
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
